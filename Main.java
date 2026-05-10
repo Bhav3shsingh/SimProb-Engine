d371a6137cc7ca40dc474b03fe340d360c8924b1
BHAV3SH <88442474+Bhav3shsingh@users.noreply.github.com>
2026-05-10 16:32:26 +0530

    Update and rename Program.java to Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main
+{
+	    public static void main(String[] args) {
+		
+SentenceMaker sen = new SentenceMaker();
+            
+		System.out.println("====================\n"+sen.sengen()+"\n=========================");
+
+	}
+}
